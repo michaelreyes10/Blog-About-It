f93bfef1ef976878b4f2a169e741e8f75ccc911f
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-10-06 16:48:40 -0400

    Create the Post Model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+//grabbing and requiring the accessable information from the models
 const User = require('./User')
+const Post = require('./Post')
 
-module.exports = { User }
+module.exports = { User, Post }
