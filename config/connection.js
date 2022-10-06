0965b7c6c68915fad8f64b55aed014be75f1930d
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-10-06 12:40:31 -0400

    completed user-model branch

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 //create the connection to the database
 //getting credentials from .env file
-const sequelize = new(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 host: 'localhost',
 dialect: 'mysql',
 port: 3306
