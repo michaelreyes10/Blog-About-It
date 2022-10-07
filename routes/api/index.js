3f4f4e36c8bb28355a59858b7d5383dd5b21e0de
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-10-07 11:55:14 -0400

    added CRUD for POST Model

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,5 +4,6 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
