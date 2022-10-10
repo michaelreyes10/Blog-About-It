984fe5a0c8ee644f5fd69c3a5272f67ebdc09178
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-10-10 15:32:00 -0400

    all routes have been updated

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment} = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -18,7 +18,16 @@ router.get('/', (req, res) => {
       {
         model: User,
         attributes: ['username']
-      }
+      },
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+    }
+      
     ]
   })
     .then(dbPostData => res.json(dbPostData))
